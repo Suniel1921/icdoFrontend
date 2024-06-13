9b2dcea3e34b66a2c1bc262f79d49d2e7c383065
sunil <logins697@gmail.com>
2024-06-13 13:39:25 +0545

    fix bug

--- src/components/cover/CoverBanner.jsx
+++ src/components/cover/CoverBanner.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
+import React from 'react';
 import '../cover/coverBanner.css';
+import { useNavigate } from 'react-router-dom';
+
 const CoverBanner = () => {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate('/contact');
+  };
+
   return (
     <>
-    <div className='coverBanner_container'>
+      <div className='coverBanner_container'>
         <div className='coverBanner_content'>
-           <div className="container">
-           <div className='content'>
-            <p className='coverBanner_para'>We are here to stop poverty</p>
-            <h2 className='coverBanner_heading'>We are fundraising for the people who are fighting against poverty</h2>
-            <button className='coverBanner_Btn'>Donate Now</button>
-
-           </div>
-           </div>
-
+          <div className="container">
+            <div className='content'>
+              <p className='coverBanner_para'>We are here to stop poverty</p>
+              <h2 className='coverBanner_heading'>We are fundraising for the people who are fighting against poverty</h2>
+              <button className='coverBanner_Btn' onClick={handleNavigate}>Contact</button>
+            </div>
+          </div>
         </div>
-    </div>
-      
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default CoverBanner
+export default CoverBanner;
